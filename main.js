a591455843c0dd7f0883ab9f8832c97786366074
KareeMagdyy <karim67899@gmail.com>
2022-11-02 01:52:19 +0200

    logic added for switching between active elem

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const collapseItems = document.querySelectorAll('[data-toggle="collapse"]');
 const optionsBtn = document.querySelectorAll('[data-toggle="options"]');
+const sideItemBody = document.querySelectorAll(".side__item-body p");
+const navLinks = document.querySelectorAll(".nav__link");
+const statusFilter = document.querySelectorAll(".status p");
+const pageNums = document.querySelectorAll(".page__nums p");
 
+toggler(navLinks, "active__link");
 toggler(collapseItems, "active");
+toggler(statusFilter, "active");
 toggler(optionsBtn, "active");
+toggler(sideItemBody, "active");
+toggler(pageNums, "active");
 
 function toggler(item, className) {
   for (i = 0; i < item.length; i++) {
